5c2574b5ae434a2bd17b9329b04e30055ba45216
Shubham Durugale <shubhamdurugale01@gmail.com>
2026-05-13 21:30:10 +0530

    Add files via upload

---
+++ static/js/yt.js
@@ -0,0 +1,81 @@
+var arr1 = ["co/po/pso", "po-1", "po-2", "po-3", "po-4", "po-5", "po-6", "po-7", "po-8", "po-9", "po-10","po-11","po-12"];
+var arr = ["co-1", "co-2", "co-3", "co-4", "co-5", "co-6", "co-7", "co-8", "co-9", "co-10"];
+var arr2 = ["pso-1", "pso-2", "pso-3", "pso-4", "pso-5", "pso-6", "pso-7", "pso-8", "pso-9", "pso-10"];
+let comb0 = ["co1/po1", "co1/po2", "co1/po3", "co1/po4", "co1/po5", "co1/po6", "co1/po7", "co1/po8", "co1/po9", "co1/po10","co1/po11", "co1/po12"];
+let comb1 = ["co2/po1", "co2/po2", "co2/po3", "co2/po4", "co2/po5", "co2/po6", "co2/po7", "co2/po8", "co2/po9", "co2/po10","co2/po11", "co2/po12"];
+let comb2 = ["co3/po1", "co3/po2", "co3/po3", "co3/po4", "co3/po5", "co3/po6", "co3/po7", "co3/po8", "co3/po9", "co3/po10","co3/po11", "co3/po12"];
+let comb3 = ["co4/po1", "co4/po2", "co4/po3", "co4/po4", "co4/po5", "co4/po6", "co4/po7", "co4/po8", "co4/po9", "co4/po10","co4/po11", "co4/po12"];
+let comb4 = ["co5/po1", "co5/po2", "co5/po3", "co5/po4", "co5/po5", "co5/po6", "co5/po7", "co5/po8", "co5/po9", "co5/po10","co5/po11", "co5/po12"];
+let comb5 = ["co6/po1", "co6/po2", "co6/po3", "co6/po4", "co6/po5", "co6/po6", "co6/po7", "co6/po8", "co6/po9", "co6/po10","co6/po11", "co6/po12"];
+let comb6 = ["co7/po1", "co7/po2", "co7/po3", "co7/po4", "co7/po5", "co7/po6", "co7/po7", "co7/po8", "co7/po9", "co7/po10","co7/po11", "co7/po12"];
+let comb7 = ["co8/po1", "co8/po2", "co8/po3", "co8/po4", "co8/po5", "co8/po6", "co8/po7", "co8/po8", "co8/po9", "co8/po10","co8/po11", "co8/po12"];
+let comb8 = ["co9/po1", "co9/po2", "co9/po3", "co9/po4", "co9/po5", "co9/po6", "co9/po7", "co9/po8", "co9/po9", "co9/po10","co9/po11", "co9/po12"];
+let comb9 = ["co10/po1", "co10/po2", "co10/po3", "co10/po4", "co10/po5", "co10/po6", "co10/po7", "co10/po8", "co10/po9", "co10/po10","co10/po11", "co10/po12"];
+
+let comb000 = ["co1/pSo1", "co1/pSo2", "co1/pSo3", "co1/pSo4", "co1/pSo5", "co1/pSo6", "co1/pSo7", "co1/pSo8", "co1/pSo9", "co1/pSo10"];
+let comb100 = ["co2/pSo1", "co2/pSo2", "co2/pSo3", "co2/pSo4", "co2/pSo5", "co2/pSo6", "co2/pSo7", "co2/pSo8", "co2/pSo9", "co2/pSo10"];
+let comb200 = ["co3/pSo1", "co3/pSo2", "co3/pSo3", "co3/pSo4", "co3/pSo5", "co3/pSo6", "co3/pSo7", "co3/pSo8", "co3/pSo9", "co3/pSo10"];
+let comb300 = ["co4/pSo1", "co4/pSo2", "co4/pSo3", "co4/pSo4", "co4/pSo5", "co4/pSo6", "co4/pSo7", "co4/pSo8", "co4/pSo9", "co4/pSo10"];
+let comb400 = ["co5/pSo1", "co5/pSo2", "co5/pSo3", "co5/pSo4", "co5/pSo5", "co5/pSo6", "co5/pSo7", "co5/pSo8", "co5/pSo9", "co5/pSo10"];
+let comb500 = ["co6/pSo1", "co6/pSo2", "co6/pSo3", "co6/pSo4", "co6/pSo5", "co6/pSo6", "co6/pSo7", "co6/pSo8", "co6/pSo9", "co6/pSo10"];
+let comb600 = ["co7/pSo1", "co7/pSo2", "co7/pSo3", "co7/pSo4", "co7/pSo5", "co7/pSo6", "co7/pSo7", "co7/pSo8", "co7/pSo9", "co7/pSo10"];
+let comb700 = ["co8/pSo1", "co8/pSo2", "co8/pSo3", "co8/pSo4", "co8/pSo5", "co8/pSo6", "co8/pSo7", "co8/pSo8", "co8/pSo9", "co8/pSo10"];
+let comb800 = ["co9/pSo1", "co9/pSo2", "co9/pSo3", "co9/pSo4", "co9/pSo5", "co9/pSo6", "co9/pSo7", "co9/pSo8", "co9/pSo9", "co9/pSo10"];
+let comb900 = ["co10/pSo1", "co10/pSo2", "co10/pSo3", "co10/pSo4", "co10/pSo5", "co10/pSo6", "co10/pSo7", "co10/pSo8", "co10/pSo9", "co10/pSo10"];
+
+let main = [comb0,comb1,comb2,comb3,comb4,comb5,comb6,comb7,comb8,comb9];
+let main0 = [comb000,comb100,comb200,comb300,comb400,comb500,comb600,comb700,comb800,comb900];
+let lastPO = [];
+let lastPSO = [];
+// let row=0;
+// let col=0;
+// let pso=0;
+
+var po= document.getElementById("po"), co=document.getElementById("co"),ps=document.getElementById("pso");
+var row =0 ,col = 0, pso=0;
+
+for( var i = 1;i<co.rows.length;i++){
+ row=parseInt(co.rows[i].cells[0].innerHTML);
+}
+for( var i = 1;i<po.rows.length;i++){
+  col=parseInt(po.rows[i].cells[0].innerHTML);
+}
+ for( var i = 1;i<ps.rows.length;i++){
+  pso=parseInt(ps.rows[i].cells[0].innerHTML);
+}
+
+// document.getElementById("pso").addEventListener("keyup",()=>{
+    //  row =  document.getElementById("row").value;
+    //  col =  document.getElementById("po").value;
+    //  pso =   document.getElementById("pso").value;
+
+    console.log(row);
+    console.log(col);
+    console.log(pso);
+   
+      let form="<form action='/poInsert/"+row+"/"+col+"/"+pso+"' method='POST'>"
+      let table="<table  border='2px solid black' id='table1'><tr class='table-success' >";
+
+      for (let k = 0; k <= col; k++) {
+        table+="<th name='"+arr1[k]+"'><h3>"+arr1[k]+"</h3></th>";
+    }
+    for (let k = 0; k < pso; k++) {
+      table+="<th name='"+arr2[k]+"'><h3>"+arr2[k]+"</h3></th>";
+    }
+      table+="</tr>";
+      for(let i=0;i<row;i++){
+        table += "<tr style='background-color:rgba(55,81,126, 0.9);color:white'>";
+        table += "<th name='"+arr[i]+"'><h3>"+arr[i]+"</h3></th>";
+        for(let j=0;j<col;j++){
+            lastPO = main[i];
+              table += "<td> <select id='select' name='"+lastPO[j]+"' style='width:'40' height:'30'' > <option value='"+0+"'>-</option> <option value='"+1+"'>1</option> <option value='"+2+"'>2</option> <option value='"+3+"'>3</option> </select></td>";
+          }
+          for(let j=0;j<pso;j++){
+            lastPSO = main0[i];
+              table += "<td> <select id='select' name='"+lastPSO[j]+"' style='width:'40' height:'30'' > <option value='"+0+"'>-</option> <option value='"+1+"'>1</option> <option value='"+2+"'>2</option> <option value='"+3+"'>3</option> </select></td>";
+          }
+        table+="</tr>";
+      }
+      table=form+table+"</table>"+"<button type='submit' id='btn' class='btn btn-primary btn-block' style='margin-top:8px;'>Submit</button>"+"</form>";
+      document.getElementById('display').innerHTML=table;
+  // });
